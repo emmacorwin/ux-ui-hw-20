dea10c43632389b69c5f6eb22dcc27de3d9a3898
emmacorwin <emmalucorwin@gmail.com>
2021-12-08 20:36:05 -0700

    update

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(".name").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+
+  $("#ux").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+  $("#graphic").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+  $("#painting").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+  $("#about").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+
+  
